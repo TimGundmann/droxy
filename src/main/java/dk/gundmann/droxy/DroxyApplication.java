a4fcc0b37ecbf238ce845ad0cbcafe09243ec4ef
Tim Gundmann <tim@gundmann.dk>
2019-04-19 12:12:34 +0200

    remove unused code

--- src/main/java/dk/gundmann/droxy/DroxyApplication.java
+++ src/main/java/dk/gundmann/droxy/DroxyApplication.java
@@ -1,8 +1,17 @@
 package dk.gundmann.droxy;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +21,19 @@ public class DroxyApplication {
 		SpringApplication.run(DroxyApplication.class, args);
 	}
 	
+	@RestController
+	@RequestMapping("/greenBlueDeploy")
+	public static class GreenBlueDeploy {
+
+		@GetMapping("/{service}/color")
+		public String getColor(@PathVariable String service) {
+			return "";
+		}
+
+		@PostMapping(value="/{service}/{color}")
+		public void updateCurrentColor(@PathVariable String service, @PathVariable String color) {
+		}
+
+	} 
+
 }
