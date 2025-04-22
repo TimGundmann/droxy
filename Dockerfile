70b20d0eceafa7ab2cf5fddd24ce2117e5712c90
Tim Gundmann <tim@gundmann.dk>
2025-04-22 12:14:44 +0200

    new server

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
-FROM openjdk:10.0.2-13-jre-sid
-
+FROM eclipse-temurin:23-jdk-alpine
 COPY target/droxy.jar /app/droxy.jar
-
 CMD ["java", "-jar", "/app/droxy.jar", "--spring.profiles.active=prod"]
--- src/main/java/dk/gundmann/droxy/DroxyApplication.java
+++ src/main/java/dk/gundmann/droxy/DroxyApplication.java
@@ -1,16 +1,9 @@
 package dk.gundmann.droxy;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @SpringBootApplication
